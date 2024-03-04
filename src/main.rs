168f722c1b90d43eb137ba555ddac9a73b708787
flpvvvv <flpvvvv@live.com>
2024-03-04 21:21:16 +0100

    feat: add struct Config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file!");
+    println!("Content in the file: \n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    println!("Query: {query}");
+    println!("From the file: {file_path}");
+    Config { query, file_path }
 }
