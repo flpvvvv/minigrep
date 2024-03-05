dabcd6f54dd680a1021b0dbd5af66f4c24fe90a8
flpvvvv <flpvvvv@live.com>
2024-03-05 20:34:16 +0100

    enh: add build func

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file!");
     println!("Content in the file: \n{contents}");
@@ -14,10 +18,13 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    println!("Query: {query}");
-    println!("From the file: {file_path}");
-    Config { query, file_path }
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
 }
