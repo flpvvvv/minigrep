3b0361187f4161c448090b6cb0f177d881f1b9ea
flpvvvv <flpvvvv@live.com>
2024-03-06 20:53:37 +0100

    test: add search test

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("The query is: '{}'", config.query);
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
