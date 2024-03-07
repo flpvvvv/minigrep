89fb7ccb9a9052561a1d1bbaab68ae2923d74729
flpvvvv <flpvvvv@live.com>
2024-03-07 20:57:30 +0100

    feat: implement search func

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,20 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{line}")
+    }
     Ok(())
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results=Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    results
 }
 
 #[cfg(test)]
